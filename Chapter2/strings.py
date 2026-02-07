5d6dc18aeca685bf95cd7330e9d1ab7f9a3cfcbd
asadirshadd <asadirshad1651@gmail.com>
2026-02-07 22:51:25 +0500

    length

--- Chapter2/strings.py
+++ Chapter2/strings.py
@@ -3,4 +3,14 @@ str1 = "AsadIrshad"
 str2 = "PythonProgramming"
 str3 = "Devops Learning"
 
-# How to check lenth of
+# How to check lenth of 
+# we use len() funtion to determince the lenth of the string
+
+print(len(str1))
+print(len(str2))
+print(len(str3))
+
+
+
+
+
