61374819f333b47d7bd3fb0ffc6c0fe4f3473f22
asadirshadd <asadirshad1651@gmail.com>
2026-02-08 01:18:00 +0500

    slicing

---
+++ Pythonlearning/slicing.py
@@ -0,0 +1,12 @@
+#In Python, slicing is an operation that allows you to extract specific portions of
+#  sequences like lists, tuples, and strings by specifying a start and ending index , 
+# and step indices with the syntax sequence[start:stop:step] .
+
+str = "United state of america"
+
+#if i want to extract the state word from the above string so we will use slicing 
+
+print(str[7:12])
+
+# we can also use the negative indexing for slicing as well
+
