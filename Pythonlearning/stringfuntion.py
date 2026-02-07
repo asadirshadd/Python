6180f3da3d2ba90088d23867494c473f440d59cc
asadirshadd <asadirshad1651@gmail.com>
2026-02-08 01:41:40 +0500

    stringfuntions

---
+++ Pythonlearning/stringfuntion.py
@@ -0,0 +1,12 @@
+str = "pakistan"
+
+print(str.endswith("er"))                      # return true/false if string ends with substr
+
+print(str.capitalize())                        # capitalize the first characher
+
+print(str.replace("pakistan","Australia"))     #replace the old character with new one 
+
+print(str.find("n"))                           # Word kis index par hai
+
+print(str.count("a"))                          # Character repeatation in string 
+
