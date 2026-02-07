f738886a0eb6860eeebe185e16d1eefc34f9dcd7
asadirshadd <asadirshad1651@gmail.com>
2026-02-07 22:57:47 +0500

    concatinate

--- Chapter2/strings.py
+++ Chapter2/strings.py
@@ -10,6 +10,16 @@ print(len(str1))
 print(len(str2))
 print(len(str3))
 
+#Concatenation is when we merge two strings we use + sign to concatinate strings
+
+name = "AsadIrshad"
+city = "Karachi"
+country = "pakistan"
+
+con = name + " " +city +" " +country
+print(con)
+print(len(con))
+
 
 
 
