aa364d47a743d8c5cd81898f033ece3ea183922e
asadirshadd <asadirshad1651@gmail.com>
2026-02-07 23:12:51 +0500

    index

---
+++ Pythonlearning/indexing.py
@@ -0,0 +1,14 @@
+#In Python, positive indexing accesses elements from the beginning of a sequence,
+# starting at index 0, while negative indexing accesses elements from the end, 
+# starting at index -1. This applies to sequence data types like lists, strings, and tuples. 
+
+
+str = "I am learnig python"
+print(str[-1])
+
+str = "I am from karachi"
+print(str[9:])
+
+list =[2,4,6,8,0]
+
+print(list[-2])
