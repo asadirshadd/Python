4c86c9818351abef1e8bf6fa8ea6184e2d70e002
asadirshadd <asadirshad1651@gmail.com>
2026-02-08 09:33:46 +0500

    list

---
+++ Pythonlearning/list.py
@@ -0,0 +1,14 @@
+# A builtin data type that stores the set of value it can store any datatype of element 
+# List in mutable means the value can be changed
+
+marks = [34,55,2,33,77,44,22,9]
+print(marks)
+print(type(marks))
+
+print(marks [1])
+
+#since the list is mutable so we can modifiy the list elements for example
+
+marks[4]=80
+print(marks)
+
