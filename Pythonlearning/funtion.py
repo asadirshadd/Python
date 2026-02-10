358733b9a0b86e556560a6bf0cabd73c0de63d5d
asadirshadd <asadirshad1651@gmail.com>
2026-02-10 19:19:23 +0500

    funtion added

---
+++ Pythonlearning/funtion.py
@@ -0,0 +1,20 @@
+#function is defined as a block of instruction which we can reuse by calling it 
+#whatever is given as input is called as parameter
+
+def sum(a,b):
+    sum = a + b
+    print(sum)
+    return sum
+
+
+# lets pass some argument and call it
+
+sum(4,5)
+
+sum(3,9)
+
+sum(9,9)
+
+
+
+
