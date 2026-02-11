9a0fc12d03981d0a03eba77324a9e1817464f79e
asadirshadd <asadirshad1651@gmail.com>
2026-02-11 14:44:14 +0500

    oop

--- Pythonlearning/funtion.py
+++ Pythonlearning/funtion.py
@@ -1,19 +1,45 @@
 #function is defined as a block of instruction which we can reuse by calling it 
+
 #whatever is given as input is called as parameter
 
+
 def sum(a,b):
+
     sum = a + b
+
     print(sum)
     return sum
 
 
+
 # lets pass some argument and call it
 
-sum(4,5)
 
-sum(3,9)
+#sum(4,5)
 
-sum(9,9)
+
+#sum(3,9)
+
+
+#sum(9,9)
+
+
+#make a funtion to create a average of 3 number
+
+
+def average(a, b, c):
+    sum = a+b+c
+    avg = sum/3
+    print(avg)
+    return avg
+
+    
+
+
+average(1,2,3)
+
+
+average(66,90,33)
 
 
 
---
+++ Pythonlearning/oop.py
@@ -0,0 +1,17 @@
+class car:
+    model = "1991"
+    maker = "suzuki"
+
+
+car1 =car()
+print(car1.maker)
+print(car1.model)
+
+
+class country:
+    city = "karachi"
+    district = "West"
+    town = "Kmc"
+
+city1 = country()
+print(city1.town)
